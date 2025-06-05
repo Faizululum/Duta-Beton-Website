aab2e7cbfe885375222faec8993e30111664fc7d
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-05 12:17:08 +0700

    chore(hero): create responsive hero layout component (#6)
    * chore(button): create responsive button component
    
    * chore(hero): create responsive hero layout component

--- src/components/layout/HeroLayout.jsx
+++ src/components/layout/HeroLayout.jsx
@@ -2,12 +2,12 @@ import Button from "../general/Button";
 
 export default function HeroLayout() {
   return (
-    <div className="flex flex-col items-start w-[558px] ml-bold-4xl gap-regular-lg">
-      <div>
-        <h2 className="text-desk-h5 font-medium uppercase text-primary-red">Duta Beton Mandiri</h2>
-        <div className="flex flex-col gap-regular-sm">
-          <h1 className="text-desk-h1 font-bold">Ready Mix</h1>
-          <p className="text-desk-tagline font-medium">
+    <div className="flex flex-col items-start w-full p-bold-sm md:p-0 md:w-[558px] lg:ml-bold-4xl gap-regular-sm md:gap-regular-lg">
+      <div className="flex flex-col">
+        <h2 className="text-mob-h5 md:text-desk-h5 font-medium uppercase text-primary-red">Duta Beton Mandiri</h2>
+        <div className="flex flex-col gap-thin-lg md:gap-regular-sm">
+          <h1 className="text-mob-h1 md:text-desk-h1 font-bold">Ready Mix</h1>
+          <p className="text-mob-tagline md:text-desk-tagline font-medium">
             Didesain dengan campuran yang sesuai takaran yang dibutuhkan, untuk
             memudahkan dan menjadikan pekerjaan pengerasan lebih cepat.
           </p>
