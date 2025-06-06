5f7a53c7713794c1a80a3a42a743aae22fa553bb
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-06 22:22:14 +0700

    feat(home): create responsive project trust section and why card component (#12)

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import HomeHero from "@/components/page/HomeHero";
+import ProjectTrust from "@/components/page/ProjectTrust";
 import WhyCard from "@/components/page/WhyCard";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <HomeHero />      
       <WhyCard />
+      <ProjectTrust />
     </div>
   );
 }
---
+++ src/components/ui/ClientCard.jsx
@@ -0,0 +1,8 @@
+export default function ClientCard() {
+  return (
+    <div className="flex flex-col w-max h-max bg-white rounded-full py-thin-sm md:py-thin-md px-regular-md  md:px-bold-sm text-center text-secondary-yellow">
+      <h2 className="text-mob-h3 md:text-desk-h1 font-medium -mb-thin-lg md:-mb-regular-sm">100+</h2>
+      <p className="text-mob-h6 md:text-desk-h5 font-normal">Client Reviews</p>
+    </div>
+  );
+}
