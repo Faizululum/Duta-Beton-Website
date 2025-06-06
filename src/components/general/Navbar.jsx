441f7c3fa27d8b4017e3708b9c5d80b219ccc618
Faizululum <faizululum25@gmail.com>
2025-06-06 11:05:37 +0700

    chore(nav): edit z-index nav

--- src/components/general/Navbar.jsx
+++ src/components/general/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from "../ui/Logo";
 
 export default function Navbar() {
   return (
-    <nav className="h-[100px] w-full flex justify-between items-center px-bold-4xl font-roboto font-normal fixed drop-shadow-md bg-white">
+    <nav className="h-[100px] w-full flex justify-between items-center px-bold-4xl font-roboto font-normal fixed drop-shadow-md bg-white z-50">
       <Logo />
       <div className="flex gap-10 items-center">
         <Link href="/">Beranda</Link>
