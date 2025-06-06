c484e4b40e97159a2a2731dc010e75fb1fc723be
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-06 21:33:45 +0700

    feat(nav): create responsive navbar and sidebar for mobile (#11)

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="flex items-center gap-2">
-      <Image
-        src="/logo.jpg"
-        className="bg-white border border-neutral-grayLight-60 p-thin-xs rounded-full"
-        alt="logo"
-        width={55}
-        height={55}
-      />
+    <div className="flex items-center gap-thin-sm md:gap-thin-md">
+      <div className="relative w-[40px] h-[40px] md:w-[55px] md:h-[55px]">
+        <Image
+          src="/logo.jpg"
+          className="bg-white border border-neutral-grayLight-60 p-thin-xs rounded-full"
+          alt="logo"
+          fill
+          sizes="(min-width: 768px) 55px, 40px"
+        />
+      </div>
       <div className="flex flex-col font-poppins uppercase text-primary-red">
-        <h1 className="font-bold text-4xl">Duta</h1>
-        <h2 className="font-normal text-2xl leading-4 tracking-[0.25em]">
+        <h1 className="font-bold text-mob-h4 md:text-desk-h4 leading-5 md:leading-7">Duta</h1>
+        <h2 className="font-normal text-mob-h6 md:text-desk-h5 leading-3 md:leading-4 tracking-[0.25em] md:tracking-[0.165em]">
           Beton
         </h2>
       </div>
