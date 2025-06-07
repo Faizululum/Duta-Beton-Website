b49b229d8505de62781717eb99295fc8796ef936
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-07 21:29:13 +0700

    chore(layout): update section using layout text component (#15)

--- src/components/page/WhyCard.jsx
+++ src/components/page/WhyCard.jsx
@@ -1,3 +1,4 @@
+import TitleLayout from "../layout/TextLayout";
 import CardValue from "../ui/CardValue";
 
 export default function WhyCard() {
@@ -5,14 +6,7 @@ export default function WhyCard() {
     <div className="h-max flex justify-center -mt-bold-lg z-10 mb-bold-3xl">
       <div className="shadow-shadow-card rounded-xl md:rounded-3xl bg-white w-[400px] md:w-max h-max pt-thin-lg md:pt-regular-lg pb-regular-lg md:p-bold-lg">
         <div className="flex flex-col gap-thin-lg">
-          <div className="flex flex-col text-center">
-            <h2 className="text-mob-h4 md:text-desk-h4 font-medium capitalize">
-              Kenapa Pilih Kami
-            </h2>
-            <p className="text-mob-tiny md:text-desk-tiny font-normal text-primary-red">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-          </div>
+          <TitleLayout title="Mengapa Pilih Kami?" description="Lorem ipsum" />
           <div className="flex flex-col md:flex-row gap-regular-sm md:gap-bold-sm w-full items-center">
             <CardValue />
             <CardValue />
