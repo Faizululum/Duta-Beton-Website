9124c63493be677b110cd47e2a1b7c5e4ac08cf0
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-07 10:57:23 +0700

    feat(text): create dynamic and responsive text layout and add new class component (#14)

---
+++ src/components/layout/TextLayout.jsx
@@ -0,0 +1,26 @@
+export default function TitleLayout({ title, description, align = "center" }) {
+  const alignmentClass =
+    align === "left" ? "text-left items-start" : "text-center items-center";
+
+  return (
+    <div className={`flex flex-col ${alignmentClass}`}>
+      <h2 className="text-mob-h4 md:text-desk-h4 font-medium capitalize">
+        {title}
+      </h2>
+      <p className="text-mob-tagline md:text-desk-tiny font-normal text-primary-red">
+        {description}
+      </p>
+    </div>
+  );
+}
+
+export function BodyLayout({ title, description }) {
+  return (
+    <div className="flex flex-col gap-thin-md md:gap-thin-lg">
+      <h1 className="text-mob-h2 font-medium md:text-desk-h2">{title}</h1>
+      <p className="text-desc">
+        {description}
+      </p>
+    </div>
+  );
+}
