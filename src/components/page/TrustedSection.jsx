e75f12106c7ad67845a808901b76f842e6ae52d4
Faizululum <faizululum25@gmail.com>
2025-06-09 09:46:24 +0700

    chore(config): add new shadow and text spacing config

--- src/components/page/TrustedSection.jsx
+++ src/components/page/TrustedSection.jsx
@@ -3,20 +3,20 @@ import TrustedCard from "../ui/TrustedCard";
 
 export default function TrustedSection() {
   return (
-    <div className="flex flex-col items-center justify-center gap-regular-lg md:gap-bold-lg w-full overflow-hidden">
+    <div className="space-text-section overflow-hidden">
       <TitleLayout
         title="Dipercaya Oleh"
         description="Trusted by the best brands in the world"
       />
-      <div className="flex flex-col gap-regular-sm md:gap-bold-lg justify-center items-center">
-        <div className="flex gap-regular-sm md:gap-bold-lg">
+      <div className="flex flex-col gap-regular-sm md:gap-bold-sm justify-center items-center">
+        <div className="flex gap-regular-sm md:gap-bold-sm">
           <TrustedCard />
           <TrustedCard />
           <TrustedCard />
           <TrustedCard />
           <TrustedCard />
         </div>
-        <div className="flex gap-regular-sm md:gap-bold-lg">
+        <div className="flex gap-regular-sm md:gap-bold-sm">
           <TrustedCard />
           <TrustedCard />
           <TrustedCard />
