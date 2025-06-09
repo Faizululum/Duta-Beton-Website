8689b440dfb5106ae5e5d6f778e39d03884e64b1
Faizululum <faizululum25@gmail.com>
2025-06-09 20:01:13 +0700

    chore(ui): add new border class component

--- src/components/page/AboutSection.jsx
+++ src/components/page/AboutSection.jsx
@@ -5,7 +5,7 @@ import CertifiedCard from "../ui/CertifiedCard";
 
 export default function AboutSection() {
   return (
-    <div className="flex flex-col bg-neutral-grayDark w-full pt-bold-lg md:pt-bold-3xl items-center mb-bold-3xl border-b-4 md:border-b-8 border-primary-red">
+    <div className="flex flex-col bg-neutral-grayDark w-full pt-bold-lg md:pt-bold-3xl items-center mb-bold-3xl border-bottom">
       <div className="flex flex-col md:flex-row-reverse w-full items-center md:justify-end mb-bold-lg">
         <CardImage src="/about.jpg" alt="about" align="right" />
         <div className="flex flex-col px-regular-lg md:pr-bold-xl w-full md:w-[60%] md:-mr-10 md:pl-32 py-thin-lg md:py-bold-sm bg-white rounded-tr-[64px] gap-regular-lg md:gap-bold-lg">
