c272d321f14cfa85d2457605416fafc749cb9042
Faizululum <faizululum25@gmail.com>
2025-06-09 21:44:03 +0700

    feat(about): create responsive about us section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,5 @@
 import AboutHero from "@/components/page/about/AboutHero";
+import AboutUsSection from "@/components/page/about/AboutUsSection";
 import DetailSection from "@/components/page/about/DetailSection";
 
 export default function AboutPage() {
@@ -6,6 +7,7 @@ export default function AboutPage() {
     <div>
       <AboutHero />
       <DetailSection />
+      <AboutUsSection />
     </div>
   );
 }
