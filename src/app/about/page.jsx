5179f0abd36ba090c87c994b2fc4266f7fd61b17
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-09 20:05:04 +0700

    feat(about): create responsive detail section (#21)
    * chore(home): update padding hero section
    
    * chore(ui): add new border class component
    
    * feat(detail): create dynamic client card for portfolio
    
    * feat(about): create responsive detail section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import AboutHero from "@/components/page/about/AboutHero";
+import DetailSection from "@/components/page/about/DetailSection";
 
 export default function AboutPage() {
   return (
     <div>
       <AboutHero />
+      <DetailSection />
     </div>
   );
 }
--- src/components/ui/ClientCard.jsx
+++ src/components/ui/ClientCard.jsx
@@ -1,8 +1,20 @@
-export default function ClientCard() {
+export default function ClientCard({
+  count = "100+",
+  label = "Client Reviews",
+  textColor = "text-secondary-yellow",
+  bgColor = "bg-white",
+  border = false,
+}) {
   return (
-    <div className="flex flex-col w-max h-max bg-white rounded-full py-thin-sm md:py-thin-md px-regular-md  md:px-bold-sm text-center text-secondary-yellow">
-      <h2 className="text-mob-h3 md:text-desk-h1 font-medium -mb-thin-lg md:-mb-regular-sm">100+</h2>
-      <p className="text-mob-h6 md:text-desk-h5 font-normal">Client Reviews</p>
+    <div
+      className={`flex flex-col w-max h-max rounded-full py-thin-sm md:py-thin-md px-regular-md md:px-bold-sm text-center ${textColor} ${bgColor} ${
+        border ? "border-2 md:border-4 border-current bg-transparent" : ""
+      }`}
+    >
+      <h2 className="text-mob-h3 md:text-desk-h1 font-medium -mb-thin-md md:-mb-regular-sm">
+        {count}
+      </h2>
+      <p className="text-mob-h6 md:text-desk-h5 font-normal">{label}</p>
     </div>
   );
 }
