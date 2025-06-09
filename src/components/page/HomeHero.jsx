ae5ddebdea10702814ad118518fc7e766e9b2509
Faizululum <faizululum25@gmail.com>
2025-06-09 13:22:40 +0700

    feat(home): add background image in home hero section

--- src/components/page/HomeHero.jsx
+++ src/components/page/HomeHero.jsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import HeroLayout from "../layout/HeroLayout";
 
 export default function HomeHero() {
   return (
-    <div className="h-screen flex items-center z-0 bg-black">
-      <HeroLayout />
+    <div className="relative h-screen w-full">
+      <Image
+        src="/hero.png"
+        alt="hero"
+        fill
+        priority
+        className="object-cover -z-10"
+      />
+      <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-t from-black/65 via-black/50 via-50% to-black/0 to-88% pointer-events-none -z-10" />
+      <div className="relative z-10 h-full flex items-center">
+        <HeroLayout />
+      </div>
     </div>
   );
 }
