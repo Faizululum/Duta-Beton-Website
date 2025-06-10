ee1a78b46e8a7bb96fe82c24193a22d82b5ad6d2
Faizululum <faizululum25@gmail.com>
2025-06-10 14:26:40 +0700

    feat(product): create responsive benefits section

--- src/app/product/concrete-block/page.jsx
+++ src/app/product/concrete-block/page.jsx
@@ -1,9 +1,11 @@
+import BenefitsSection from "@/components/page/product/BenefitsSection";
 import ProductHero from "@/components/page/product/ProductHero";
 
 export default function ConcreteBlockPage() {
   return (
     <div>
         <ProductHero />
+        <BenefitsSection />
     </div>
   );
 }
