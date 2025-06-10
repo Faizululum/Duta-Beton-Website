a1e38fa729198a973380eccdfc89317696fd28d0
Faizululum <faizululum25@gmail.com>
2025-06-10 10:03:42 +0700

    feat(about): create responsive vision mission section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,7 @@
 import AboutHero from "@/components/page/about/AboutHero";
 import AboutUsSection from "@/components/page/about/AboutUsSection";
 import DetailSection from "@/components/page/about/DetailSection";
+import MissionSection from "@/components/page/about/MissionSection";
 
 export default function AboutPage() {
   return (
@@ -8,6 +9,7 @@ export default function AboutPage() {
       <AboutHero />
       <DetailSection />
       <AboutUsSection />
+      <MissionSection />
     </div>
   );
 }
