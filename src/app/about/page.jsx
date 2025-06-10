c6c520af25ca5f87a19bed55c7818d60f136aa88
Faizululum <faizululum25@gmail.com>
2025-06-10 10:09:05 +0700

    feat(about): add responsive trusted section component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,7 @@ import AboutHero from "@/components/page/about/AboutHero";
 import AboutUsSection from "@/components/page/about/AboutUsSection";
 import DetailSection from "@/components/page/about/DetailSection";
 import MissionSection from "@/components/page/about/MissionSection";
+import TrustedSection from "@/components/page/TrustedSection";
 
 export default function AboutPage() {
   return (
@@ -10,6 +11,7 @@ export default function AboutPage() {
       <DetailSection />
       <AboutUsSection />
       <MissionSection />
+      <TrustedSection />
     </div>
   );
 }
