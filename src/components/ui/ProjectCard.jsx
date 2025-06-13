961592af66b3eb8d8f8322727324f8d1f2f047f0
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-13 13:19:24 +0700

    feat(project): create dynamic project detail card component (#33)
    * chore(project): add responsive overflow scroll behaviour
    
    * feat(calc): create responsive and dynamic calculator card component
    
    * refactor(category): add new props for filtering
    
    * feat(calc): create responsive and dynamic image calc component
    
    * feat(page): create responsive and dynamic calculator page
    
    * feat(project): create dynamic and new project image
    
    * feat(project): add dynamic project card component
    
    * feat(project): create dynamic project detail card component

--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,28 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function ProjectCard() {
+export default function ProjectCard({
+  title,
+  location,
+  images,
+  slug,
+}) {
   return (
     <div className="w-[376px] shadow-shadow-card-small p-regular-md bg-white border-2 border-neutral-white rounded-xl">
       <div className="flex flex-col gap-thin-md">
         <div className="w-full h-[200px] relative">
           <Image
-            src="/hero.png"
-            alt="project"
+            src={images}
+            alt={title}
             fill
             className="object-cover rounded-lg"
           />
         </div>
         <div className="flex w-full items-center justify-between">
           <div className="flex flex-col -space-y-thin-sm">
-            <h4 className="text-desk-tagline font-bold">
-              Saygon Inn & Cottage
-            </h4>
+            <h4 className="text-desk-tagline font-bold">{title}</h4>
             <p className="text-desk-tiny font-normal text-primary-red">
-              Purwosari - Kab. Pasuruan, Jatim
+              {location}
             </p>
           </div>
-          <Link href="/project/{slug}" className="w-bold-sm h-bold-sm relative">
+          <Link href={`/project/${slug}`} className="w-bold-sm h-bold-sm relative">
             <Image src="/icons/arrow_red.svg" alt="arrow" fill />
           </Link>
         </div>
--- src/components/ui/ProjectImage.jsx
+++ src/components/ui/ProjectImage.jsx
@@ -1,14 +1,32 @@
 import Image from "next/image";
 
-export default function ProjectImage() {
+export default function ProjectImageSmall({ src }) {
   return (
     <div className="w-[85px] md:w-[206px] h-[52px] md:h-[126px] relative">
       <Image
-        src="/hero.png"
+        src={src}
         alt="project"
         fill
         className="object-cover rounded-md md:rounded-lg"
       />
+    </div>
+  );
+}
+
+export function ProjectImageLarge({ image, title, location }) {
+  return (
+    <div className="w-full md:w-[900px] h-[230px] md:h-[552px] relative rounded-lg">
+      <Image
+        src={image}
+        alt="project"
+        fill
+        className="object-cover rounded-lg md:rounded-2xl"
+      />
+      <div className="absolute rounded-lg md:rounded-2xl bottom-0 left-0 w-full h-full bg-gradient-to-t from-black/45 via-black/25 via-45% to-black/0 to-65% pointer-events-none" />
+      <div className="flex flex-col absolute text-white w-full h-full justify-end px-thin-lg md:px-bold-sm py-thin-md md:py-regular-lg">
+        <h4 className="text-mob-h5 md:text-desk-h3 font-medium">{title}</h4>
+        <p className="text-mob-h6 md:text-desk-h5 font-normal">{location}</p>
+      </div>
     </div>
   );
 }
