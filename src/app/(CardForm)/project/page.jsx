f050ce29ffc76683570647813f1155a36c5f69cf
Faizululum <faizululum25@gmail.com>
2025-06-15 20:34:02 +0700

    feat(project): add filtering project by product category

--- src/app/(CardForm)/project/page.jsx
+++ src/app/(CardForm)/project/page.jsx
@@ -14,8 +14,7 @@ const categories = [
   "Stenslag",
 ];
 
-export default function ProjectDetailPage() {
-
+export default function ProjectPage() {
   const [activeCategory, setActiveCategory] = useState(categories[0]);
 
   return (
@@ -23,7 +22,7 @@ export default function ProjectDetailPage() {
       <h1 className="text-mob-h2 md:text-mob-h2 font-bold text-white">
         Proyek
       </h1>
-      <div className="flex flex-col md:w-[1226px] gap-regular-sm md:gap-bold-sm card-container p-thin-lg md:p-bold-lg md:pt-regular-lg pb-regular-lg">
+      <div className="flex flex-col md:w-[1256px] gap-regular-sm md:gap-bold-sm card-container p-thin-lg md:p-bold-lg md:pt-regular-lg pb-regular-lg">
         <div className="flex flex-col gap-thin-lg md:gap-regular-lg">
           <TitleLayout title="Proyek Telah Selesai" />
           <CategoryCard
@@ -32,16 +31,21 @@ export default function ProjectDetailPage() {
             setActiveCategory={setActiveCategory}
           />
         </div>
-        <div className="w-full flex flex-col md:flex-row gap-thin-lg md:gap-regular-lg flex-wrap">
-          {projects.map((project, index) => (
-            <ProjectCard
-              key={index}
-              title={project.title}
-              location={project.location}
-              images={project.images[0]}
-              slug={project.slug}
-            />
-          ))}
+        <div className="w-full flex flex-col md:grid md:grid-cols-3 gap-thin-lg md:gap-regular-sm flex-wrap">
+          {projects
+            .filter((project) => {
+              if (activeCategory === "Semua Proyek") return true;
+              return project.product.includes(activeCategory);
+            })
+            .map((project, index) => (
+              <ProjectCard
+                key={index}
+                title={project.title}
+                location={project.location}
+                images={project.images[0]}
+                slug={project.slug}
+              />
+            ))}
         </div>
       </div>
     </div>
