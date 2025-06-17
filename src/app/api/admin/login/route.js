2c5cb1477f15270cc23c2e6b5658be5139a991fb
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-17 18:25:20 +0700

    feat(auth): create token auth middleware using jwt token (#45)

--- src/app/api/admin/login/route.js
+++ src/app/api/admin/login/route.js
@@ -1,13 +1,15 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 const prisma = new PrismaClient();
 
 export async function POST(req) {
   const { username, password } = await req.json();
-
   const admin = await prisma.admin.findUnique({ where: { username } });
+
   if (!admin) {
     return NextResponse.json({ error: "User tidak ditemukan" }, { status: 401 });
   }
@@ -17,14 +19,18 @@ export async function POST(req) {
     return NextResponse.json({ error: "Password salah" }, { status: 401 });
   }
 
-  const res = NextResponse.json({ message: "Login sukses" });
+  // Generate JWT
+  const token = jwt.sign({ id: admin.id, username: admin.username }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 
-  res.cookies.set("admin-token", "bolehmasuk", {
-    path: "/",
+  // Simpan token di cookie
+  cookies().set("admin-token", token, {
     httpOnly: true,
+    path: "/",
     maxAge: 60 * 60 * 24,
     sameSite: "strict",
   });
 
-  return res;
+  return NextResponse.json({ message: "Login sukses" });
 }
--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,27 @@
 import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+const SECRET = process.env.JWT_SECRET || "dbmsecretkey";
 
 export function middleware(req) {
   const token = req.cookies.get("admin-token")?.value;
   const url = req.nextUrl;
 
-  if (!token && url.pathname.startsWith("/admin") && url.pathname !== "/admin/login") {
-    return new NextResponse(null, { status: 404 });
+  if (url.pathname.startsWith("/admin") && url.pathname !== "/admin/login") {
+    try {
+      jwt.verify(token, SECRET);
+    } catch (err) {
+      return new NextResponse(null, { status: 404 });
+    }
   }
 
   if (token && url.pathname === "/admin/login") {
-    return NextResponse.redirect(new URL("/admin/project", req.url));
+    try {
+      jwt.verify(token, SECRET);
+      return NextResponse.redirect(new URL("/admin/project", req.url));
+    } catch (err) {
+      return NextResponse.redirect(new URL("/admin/login", req.url));
+    }
   }
 
   return NextResponse.next();
