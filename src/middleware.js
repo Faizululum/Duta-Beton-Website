cc971c6d95771e0db1dfe5870b66cce60fdd2b36
Faizululum <faizululum25@gmail.com>
2025-06-17 11:01:47 +0700

    feat(auth): create middleware admin route and install bcrypt for hash password

---
+++ src/middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const token = req.cookies.get("admin-token")?.value;
+  const url = req.nextUrl;
+
+  if (!token && url.pathname.startsWith("/admin") && url.pathname !== "/admin/login") {
+    return new NextResponse(null, { status: 404 });
+  }
+
+  if (token && url.pathname === "/admin/login") {
+    return NextResponse.redirect(new URL("/admin/project", req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin/:path*"],
+};
