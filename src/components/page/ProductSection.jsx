a0f4ee2901d88b505a2c73110ad61ad25fcc8a6e
Faizululum <faizululum25@gmail.com>
2025-06-18 10:09:18 +0700

    feat(ProductCard): render card content dynamically based on product type

--- src/components/page/ProductSection.jsx
+++ src/components/page/ProductSection.jsx
@@ -2,6 +2,33 @@ import TitleLayout from "../layout/TextLayout";
 import ProductCard from "../ui/ProductCard";
 
 export default function ProductSection() {
+  const products = [
+    {
+      title: "Ready Mix",
+      description: "Beton cor dengan mutu K100 sampai dengan K500",
+      imageSrc: "/product/ready_mix.svg",
+      slug: "ready-mix",
+    },
+    {
+      title: "Precast Concrete",
+      description: "Berbagai varian uditch & precast",
+      imageSrc: "/product/precast_concrete.svg",
+      slug: "precast-concrete",
+    },
+    {
+      title: "Concrete Block",
+      description: "Produk berupa paving, topi uskup, kansteen, & batako.",
+      imageSrc: "/product/concrete_block.svg",
+      slug: "concrete-block",
+    },
+    {
+      title: "Stenslag",
+      description: "Batu pecah dengan berbagai ukuran",
+      imageSrc: "/product/stenslag.svg",
+      slug: "stenslag",
+    },
+  ];
+
   return (
     <div className="space-text-section px-regular-lg">
       <TitleLayout
@@ -9,10 +36,15 @@ export default function ProductSection() {
         description="Kami menyediakan produk yang berkualitas dan bertahan lama."
       />
       <div className="flex flex-col md:flex-row gap-regular-sm md:gap-bold-lg items-center">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((product) => (
+          <ProductCard
+            key={product.title}
+            title={product.title}
+            description={product.description}
+            imageSrc={product.imageSrc}
+            slug={product.slug}
+          />
+        ))}
       </div>
     </div>
   );
