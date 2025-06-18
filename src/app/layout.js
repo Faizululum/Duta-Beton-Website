ae34620ea7f174b3a924eb65e4773029fb9f54d2
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-18 08:24:13 +0700

    feat(nav): create responsive and dynamic transparant nav condition style (#47)
    * feat(nav): create responsive and dynamic transparant nav condition style
    
    * feat(nav): create responsive dropdown menu nav product

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import Navbar from "@/components/general/Navbar";
-import "./globals.css";
+import NavbarWrapper from "@/components/general/NavbarWrapper";
 import Footer from "@/components/general/Footer";
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 export const metadata = {
   title: "Duta Beton Mandiri",
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="font-roboto">
-        <Navbar />
+        <NavbarWrapper />
         <Toaster position="top-right" reverseOrder={false} />
         {children}
         <Footer />
