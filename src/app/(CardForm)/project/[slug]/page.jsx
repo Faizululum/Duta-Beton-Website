97b25313f094797c8bc71dd2847a9b6dfbac55cb
Faizululum <149987871+Faizululum@users.noreply.github.com>
2025-06-20 15:01:41 +0700

    deployment (#57)

--- src/app/(CardForm)/project/[slug]/page.jsx
+++ src/app/(CardForm)/project/[slug]/page.jsx
@@ -4,7 +4,7 @@ import ProjectDetailContent from "@/components/page/project/ProjectDetailContent
 export default async function ProjectDetailPage({ params }) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/projects/${params.slug}`,
-    { cache: "no-store" }
+    { cache: "force-cache" }
   );
 
   if (!res.ok) return notFound();
