58ad438f3b8f51e2de6d30de7c257fbbe34dde92
Faizululum <faizululum25@gmail.com>
2025-06-20 11:22:49 +0700

    feat(project-detail): make main image dynamic on thumbnail click with transition

--- src/components/ui/ProjectDetailCard.jsx
+++ src/components/ui/ProjectDetailCard.jsx
@@ -1,20 +1,27 @@
-export default function ProjectDetailCard({ title, product, location, periodStart, periodEnd, description }) {
+export default function ProjectDetailCard({
+  title,
+  product,
+  location,
+  periodStart,
+  periodEnd,
+  description,
+}) {
   return (
     <div className="flex flex-col md:flex-row gap-regular-sm md:gap-bold-sm w-full">
       <div className="flex flex-col gap-thin-lg p-thin-lg bg-white md:w-[376px] rounded-xl shadow-shadow-card-small">
         <div className="flex flex-col gap-thin-lg">
           <div className="flex flex-col -space-y-thin-sm px-thin-lg md:px-regular-md">
             <h4 className="text-mob-h4 font-medium">Detail Proyek</h4>
-            <p className="text-mob-h6 font-normal text-primary-red">
-              {title}
-            </p>
+            <p className="text-mob-h6 font-normal text-primary-red">{title}</p>
           </div>
           <div className="border-space-gray" />
         </div>
         <div className="flex flex-col gap-thin-md px-thin-lg md:px-regular-md">
           <div className="flex flex-col -space-y-thin-xs">
             <p className="text-mob-tagline text-primary-red">Produk</p>
-            <h6 className="text-mob-h6 font-medium">{product.map((p) => p.replace("_", " ")).join(", ")}</h6>
+            <h6 className="text-mob-h6 font-medium">
+              {product.map((p) => p.replace("_", " ")).join(", ")}
+            </h6>
           </div>
           <div className="border-space-gray border-t-0" />
           <div className="flex flex-col -space-y-thin-xs">
@@ -23,9 +30,24 @@ export default function ProjectDetailCard({ title, product, location, periodStar
           </div>
           <div className="border-space-gray border-t-0" />
           <div className="flex flex-col -space-y-thin-xs">
-            <p className="text-mob-tagline text-primary-red">Waktu Pengerjaan</p>
-            <h6 className="text-mob-h6 font-medium">{new Date(periodStart).toLocaleDateString()} - {new Date(periodEnd).toLocaleDateString()}</h6>
+            <p className="text-mob-tagline text-primary-red">
+              Waktu Pengerjaan
+            </p>
+            <h6 className="text-mob-h6 font-medium">
+              {new Date(periodStart).toLocaleDateString("id-ID", {
+                day: "2-digit",
+                month: "short",
+                year: "numeric",
+              })}{" "}
+              -{" "}
+              {new Date(periodEnd).toLocaleDateString("id-ID", {
+                day: "2-digit",
+                month: "short",
+                year: "numeric",
+              })}
+            </h6>
           </div>
+
           <div className="border-space-gray border-t-0" />
         </div>
       </div>
@@ -33,16 +55,12 @@ export default function ProjectDetailCard({ title, product, location, periodStar
         <div className="flex flex-col gap-thin-lg">
           <div className="flex flex-col -space-y-thin-sm px-thin-lg md:px-regular-md">
             <h4 className="text-mob-h4 font-medium">Deskripsi Proyek</h4>
-            <p className="text-mob-h6 font-normal text-primary-red">
-              {title}
-            </p>
+            <p className="text-mob-h6 font-normal text-primary-red">{title}</p>
           </div>
           <div className="border-space-gray" />
         </div>
         <div className="flex flex-col gap-thin-md px-thin-lg md:px-regular-md">
-          <p className="text-desc">
-            {description}
-          </p>
+          <p className="text-desc">{description}</p>
         </div>
       </div>
     </div>
