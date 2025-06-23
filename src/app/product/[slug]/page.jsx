323cd741775a7e39f4356b889a9fd5019cb414eb
Faizululum <faizululum25@gmail.com>
2025-06-23 08:45:56 +0700

    chore(product): add new ui for product category

--- src/app/product/[slug]/page.jsx
+++ src/app/product/[slug]/page.jsx
@@ -5,6 +5,7 @@ import SpecsSection from "@/components/page/product/SpecsSection";
 import { notFound } from "next/navigation";
 import { allSpecsData } from "@/lib/data/specsData";
 import BenefitsSection from "@/components/page/product/BenefitsSection";
+import TrustedSection from "@/components/page/TrustedSection";
 
 export async function generateMetadata({ params }) {
   const slug = params.slug;
@@ -24,18 +25,24 @@ export async function generateMetadata({ params }) {
 }
 
 export default async function ProductPage({ params }) {
-  const slug = await params.slug;
+  const slug = params.slug;
   const data = productData[slug];
   const specsData = allSpecsData[slug];
 
-  if (!data || !specsData) return notFound();
+  if (!data) return notFound();
+
+  const isSimpleProduct = slug === "ready-mix" || slug === "stenslag";
 
   return (
     <>
       <ProductHero data={data.hero} />
       <AboutProductSection data={data.about} />
       <BenefitsSection title={data.hero} />
-      <SpecsSection data={specsData} title={data.hero} />
+      {isSimpleProduct ? (
+        <TrustedSection />
+      ) : (
+        <SpecsSection data={specsData} title={data.hero} />
+      )}
     </>
   );
 }
